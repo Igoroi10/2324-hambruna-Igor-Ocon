54a18e8dfe34669825b06c325c977fee6d9c4bdd
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-09-18 19:28:52 +0200

    [NEW] most ironed donut function

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 
 const sugarDonut = (filteredDonuts) =>{
     let mostSugar = 0;
-    filteredDonuts.map(el =>{
+    filteredDonuts.forEach(el =>{
         if(parseInt(el.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars) > mostSugar)
         mostSugar = parseInt(el.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars);
         }
@@ -12,7 +12,19 @@ const sugarDonut = (filteredDonuts) =>{
 }
 
 const ironDonut = (filteredDonuts) =>{
+    let mostIron = 0;
+    let mostIronName = "";
+    filteredDonuts.map(el => {
+        el.nutrition_facts.nutrition.vitamines.forEach(element => {
+            if(element.type === "Iron" && parseInt(element.percent) > mostIron){
+                mostIron = parseInt(element.percent);
+                mostIronName = el.name;
+            }
 
+        })
+    });
+
+    console.log("El donut con más hierro es " + mostIronName);
 }
 
 
